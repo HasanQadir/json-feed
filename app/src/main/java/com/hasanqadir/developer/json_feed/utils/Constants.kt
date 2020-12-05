bd3a6253294df53f0fe2bd62ea67f04f9b982349
Hasan Qadir <hasan.qadir@gmail.com>
2020-12-05 22:47:00 +0500

    1) Created/Modified Constants file to define different constants. 2) Added strings in strings.xml file.

--- app/src/main/java/com/hasanqadir/developer/json_feed/utils/Constants.kt
+++ app/src/main/java/com/hasanqadir/developer/json_feed/utils/Constants.kt
@@ -1,5 +1,12 @@
 package com.hasanqadir.developer.json_feed.utils
 
 object Constants {
-    const val API_BASE_URL: String = "https://www.googleapis.com/books"
+    const val API_BASE_URL: String = "https://www.googleapis.com/books/"
+    const val VOLUME_AUTHOR = "inauthor:"
+}
+
+object NetworkConstants {
+    private const val V1 = "v1/"
+    private const val VOLUMES = "volumes"
+    const val GET_VOLUMES = V1.plus(VOLUMES)
 }
